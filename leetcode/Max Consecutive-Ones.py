be35a962ddb81734cb8d10c8d983e3e94872f9c9
Hyungil Ed Kim <79838132+hyungilk@users.noreply.github.com>
2021-03-26 23:09:47 +0900

    Update Max Consecutive-Ones.py

--- leetcode/Max Consecutive-Ones.py
+++ leetcode/Max Consecutive-Ones.py
@@ -1,5 +1,19 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_num = 0
+        cnt = 0
+        
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                cnt += 1
+                max_num = max(max_num, cnt)
+            else:
+                cnt = 0
+        return max_num
+                
+'''
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
         if nums[0] == 1:
             cnt = 1
         else:
@@ -21,4 +35,4 @@ class Solution:
                 cnt = 0
                 cnt_list.append(cnt)
         return max(cnt_list)
-                
+'''            
